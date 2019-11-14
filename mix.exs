9a2c859681ecd9d95a05fdd7fa2bb196f945d606
niku <niku@niku.name>
2019-11-14 18:36:38 +0900

    Use credo

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule EarmarkHashedLink.MixProject do
     [
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
-      {:credo, "~> 1.1.0", only: :dev, runtime: false}
+      {:credo, "~> 1.1.0", only: :dev, runtime: false},
+      {:earmark, "~> 1.4"}
     ]
   end
 end
